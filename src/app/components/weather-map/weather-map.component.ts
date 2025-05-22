13e941e497dd20ce839a29927930f25594a7679c
Vrushabh Ghawade <vrushabh.ghawade07@gmail.com>
2025-05-22 13:17:29 +0530

    code commiteed

---
+++ src/app/components/weather-map/weather-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-map.component.html',
+  styleUrl: './weather-map.component.scss'
+})
+export class WeatherMapComponent {
+
+}
