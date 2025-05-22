988fa8a5fece7d326f3670167417c269325720da
Vrushabh Ghawade <vrushabh.ghawade07@gmail.com>
2025-05-22 13:42:59 +0530

    code commited

--- src/app/components/weather-map/weather-map.component.ts
+++ src/app/components/weather-map/weather-map.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-weather-map',
-  standalone: true,
-  imports: [],
-  templateUrl: './weather-map.component.html',
-  styleUrl: './weather-map.component.scss'
+    selector: 'app-weather-map',
+    imports: [],
+    templateUrl: './weather-map.component.html',
+    styleUrl: './weather-map.component.scss'
 })
 export class WeatherMapComponent {
 
